bf8ff9528071a3d0afef63614788ec0d28cad902
Denis Vakar <nuclbain@gmail.com>
2023-07-09 19:17:46 +0300

    Added Stormguard common armors

--- Module_system 1.171/module_items.py
+++ Module_system 1.171/module_items.py
@@ -1519,6 +1519,27 @@ items = [
 ["ms_facemask_a", "Tempest Sentinels Facemask", [("mask_1",0)], itp_merchandise|itp_type_head_armor, 0, 1099, weight(2)|abundance(100)|head_armor(55)|body_armor(0)|leg_armor(0)|difficulty(8) ,imodbits_plate ],
 ["ms_facemask_b", "Tempest Sentinels Facemask", [("mask_2",0)], itp_merchandise|itp_type_head_armor, 0, 1099, weight(2)|abundance(100)|head_armor(55)|body_armor(0)|leg_armor(0)|difficulty(8) ,imodbits_plate ],
 
+["ms_gambeson_a", "Stormguard Gambeson", [("light_gambeson_a",0)], itp_merchandise|itp_type_body_armor, 0, 499, weight(15)|abundance(100)|head_armor(0)|body_armor(24)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+["ms_gambeson_b", "Stormguard Gambeson", [("light_gambeson_b",0)], itp_merchandise|itp_type_body_armor, 0, 499, weight(15)|abundance(100)|head_armor(0)|body_armor(24)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+["ms_gambeson_c", "Stormguard Gambeson", [("light_gambeson_c",0)], itp_merchandise|itp_type_body_armor, 0, 499, weight(15)|abundance(100)|head_armor(0)|body_armor(24)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+
+["ms_byrnie_a", "Stormguard Byrnie", [("english_byrne",0)], itp_merchandise|itp_type_body_armor, 0, 899, weight(20)|abundance(100)|head_armor(0)|body_armor(36)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+["ms_byrnie_b", "Stormguard Byrnie", [("viking_byrnie_b",0), ("viking_byrnie_b.1",0)], itp_merchandise|itp_type_body_armor, 0, 899, weight(20)|abundance(100)|head_armor(0)|body_armor(36)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+["ms_byrnie_c", "Stormguard Byrnie", [("viking_byrnie_c",0)], itp_merchandise|itp_type_body_armor, 0, 899, weight(20)|abundance(100)|head_armor(0)|body_armor(36)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+["ms_byrnie_d", "Stormguard Byrnie", [("viking_byrnie_new",0)], itp_merchandise|itp_type_body_armor, 0, 899, weight(20)|abundance(100)|head_armor(0)|body_armor(36)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+
+["ms_mail_shirt_a", "Stormguard Mail Shirt", [("mail_shirt_viking",0)], itp_merchandise|itp_type_body_armor, 0, 1299, weight(23)|abundance(100)|head_armor(0)|body_armor(48)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+["ms_mail_shirt_b", "Stormguard Mail Shirt", [("mail_shirt_viking_blue",0)], itp_merchandise|itp_type_body_armor, 0, 1299, weight(23)|abundance(100)|head_armor(0)|body_armor(48)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+["ms_mail_shirt_c", "Stormguard Mail Shirt", [("mail_shirt_viking_green",0)], itp_merchandise|itp_type_body_armor, 0, 1299, weight(23)|abundance(100)|head_armor(0)|body_armor(48)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+
+["ms_short_hauberk_a", "Stormguard Short Hauberk", [("norman_short_hauberk",0)], itp_merchandise|itp_type_body_armor, 0, 1299, weight(22)|abundance(100)|head_armor(0)|body_armor(48)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+["ms_short_hauberk_b", "Stormguard Short Hauberk", [("norman_short_hauberk_blue",0)], itp_merchandise|itp_type_body_armor, 0, 1299, weight(22)|abundance(100)|head_armor(0)|body_armor(48)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+["ms_short_hauberk_c", "Stormguard Short Hauberk", [("norman_short_hauberk_yellow",0)], itp_merchandise|itp_type_body_armor, 0, 1299, weight(22)|abundance(100)|head_armor(0)|body_armor(48)|leg_armor(0)|difficulty(5) ,imodbits_cloth ],
+
+["ms_stormbringer_hauberk_a", "Stormbringer Hauberk", [("saxon_hauberk_a",0)], itp_merchandise|itp_type_body_armor, 0, 1599, weight(25)|abundance(100)|head_armor(0)|body_armor(52)|leg_armor(0)|difficulty(8) ,imodbits_cloth ],
+["ms_stormbringer_hauberk_b", "Stormbringer Hauberk", [("saxon_hauberk_b",0)], itp_merchandise|itp_type_body_armor, 0, 1599, weight(25)|abundance(100)|head_armor(0)|body_armor(52)|leg_armor(0)|difficulty(8) ,imodbits_cloth ],
+["ms_stormbringer_hauberk_c", "Stormbringer Hauberk", [("saxon_hauberk_c",0)], itp_merchandise|itp_type_body_armor, 0, 1599, weight(25)|abundance(100)|head_armor(0)|body_armor(52)|leg_armor(0)|difficulty(8) ,imodbits_cloth ],
+
 # black horde items
 ["mh_helmet_heavy_a", "Heavy Hairako Helmet", [("mask_helmet1",0), ("mask_helmet1.1",0)], itp_merchandise|itp_type_head_armor, 0, 899, weight(2)|abundance(100)|head_armor(50)|body_armor(0)|leg_armor(0)|difficulty(8) ,imodbits_plate ],
 
