262e242840dd09f4de855cd0f48fbcdc86df91e1
Denis Vakar <nuclbain@gmail.com>
2024-06-03 20:18:34 +0300

    Split troops into files

--- Module_system 1.171/troops/module_troops_utils_merge.py
+++ Module_system 1.171/troops/module_troops_utils_merge.py
@@ -4,6 +4,12 @@ from module_troops_merge_factions import *
 from module_troops_merge_outlaws import *
 from module_troops_merge_manhunters import *
 from module_troops_merge_summoned import *
+from module_troops_merge_female import *
+from module_troops_merge_walkers import *
+from module_troops_merge_tutorial import *
+from module_troops_merge_tavern import *
+from module_troops_merge_npc import *
+from module_troops_merge_kings import *
 from module_troops_merge_undefined import *
 
 troops = (
@@ -13,5 +19,11 @@ troops = (
     + troops_outlaws
     + troops_manhunters
     + troops_summoned
+    + troops_female
+    + troops_walkers
+    + troops_tutorial
+    + troops_tavern
+    + troops_npc
+    + troops_kings
     + troops_undefined
 )
