5027b1da8921b3fe88a515b0a7564e3379944f08
Denis Vakar <nuclbain@gmail.com>
2024-06-03 19:38:07 +0300

    Started troops refactoring by splitting logic between files

---
+++ Module_system 1.171/troops/module_troops_utils_merge.py
@@ -0,0 +1,6 @@
+from module_troops_merge_neutral import *
+from module_troops_merge_merc import *
+from module_troops_merge_factions import *
+from module_troops_merge_undefined import *
+
+troops = troops_neutral + troops_mercenaries + troops_factions + troops_undefined
