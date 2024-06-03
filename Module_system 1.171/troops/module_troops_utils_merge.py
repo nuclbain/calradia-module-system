ca51253932a7ce94d2d6591d6b8aef62e92fd80f
Denis Vakar <nuclbain@gmail.com>
2024-06-03 19:46:19 +0300

    Add new split troops modules

--- Module_system 1.171/troops/module_troops_utils_merge.py
+++ Module_system 1.171/troops/module_troops_utils_merge.py
@@ -1,6 +1,17 @@
 from module_troops_merge_neutral import *
 from module_troops_merge_merc import *
 from module_troops_merge_factions import *
+from module_troops_merge_outlaws import *
+from module_troops_merge_manhunters import *
+from module_troops_merge_summoned import *
 from module_troops_merge_undefined import *
 
-troops = troops_neutral + troops_mercenaries + troops_factions + troops_undefined
+troops = (
+    troops_neutral
+    + troops_mercenaries
+    + troops_factions
+    + troops_outlaws
+    + troops_manhunters
+    + troops_summoned
+    + troops_undefined
+)
